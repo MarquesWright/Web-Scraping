cf0c6135c3c04f2d317f064788ab659904377783
MarquesWright <wright.marques@gmail.com>
2024-03-25 00:25:29 -0400

    added a few examples for for_all() method

--- Soup/ecologicalpyramid.py
+++ Soup/ecologicalpyramid.py
@@ -30,6 +30,7 @@ with open("ecologicalpyramid.html", "r") as ecological_pyramid:
     # secondary_consumer = soup.find(is_secondary_consumers)
 
     # Searching with find_all() method
+    # find_all() method has extra parameter, limit
 
     # finding all tertiary consumers
     # all_tertiaryconsumers = soup.find_all(class_="tertiaryconsumerlist")
