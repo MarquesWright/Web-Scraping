45daeb4222c47c18d4528cbf197f63d0c980a890
MarquesWright <wright.marques@gmail.com>
2024-03-30 21:25:02 -0400

    added a few examples for for_all() method

--- Soup/ecologicalpyramid.py
+++ Soup/ecologicalpyramid.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 with open("ecologicalpyramid.html", "r") as ecological_pyramid:
     soup = BeautifulSoup(ecological_pyramid, "lxml")
 
-    # Searching with find() method
+    # SEARCHING USING find() METHOD
 
     # finding the producers within the <ul> tag using the find() method
     # producer_entries = soup.find("ul")
@@ -18,18 +18,18 @@ with open("ecologicalpyramid.html", "r") as ecological_pyramid:
     # searching based on attribute values of a tag
     # primary_consumers = soup.find(id="primaryconsumers")
 
-    # searching based on the CSS class
+    # Searching based on the CSS class
     # css_class = soup.find(attrs={"class": "primaryconsumerlist"})
     # css_class = soup.find(class_="primaryconsumerlist")     # using the 'class_' keyword argument
 
-    # searching using functions defined
+    # Searching using functions defined
     # finding the secondary consumers using functions within the find() method
     # def is_secondary_consumers(tag):
     #     return tag.has_attr('id') and tag.get('id') == 'secondaryconsumers'
 
     # secondary_consumer = soup.find(is_secondary_consumers)
 
-    # Searching with find_all() method
+    # SEARCHING WITH find_all() METHOD
     # find_all() method has extra parameter, limit, which limits the number of results you get
 
     # finding all tertiary consumers
@@ -38,6 +38,16 @@ with open("ecologicalpyramid.html", "r") as ecological_pyramid:
     # iterate through this list to display all tertiary consumer names
     # for tertiaryconsumer in all_tertiaryconsumers:
     #     print(tertiaryconsumer.div.string)
+
+    # Understanding parameters used with find_all()
+
+    # searching for all text within the document
+    # all_texts = soup.find_all(text=True)
+
+    # searching for a list of strings
+    all_texts_in_list = soup.find_all(text=["plants", "algae"])
+
+    # PRINT METHODS FOR DISPLAYING EXAMPLES
 
     # prints the text stored in the <div> element within the <li> element
     # print(producer_entries.li.div.string)
@@ -59,3 +69,12 @@ with open("ecologicalpyramid.html", "r") as ecological_pyramid:
 
     # prints the type of the variable
     # print(type(all_tertiaryconsumers))
+
+    # prints out all the tags associated with soup object
+    # print(soup.find_all(True))
+
+    # prints every text content within the soup object including the new-line characters
+    # print(all_texts)
+
+    # prints strings that are defined in a list
+    print(all_texts_in_list)
